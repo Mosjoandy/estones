31d097a435294fd4209a51835c7b4590e994aa36
ItzDeme <61122692+ItzDeme@users.noreply.github.com>
2022-09-22 18:34:18 -0400

    Added standalone page
    Created Estone2, based off the original page to route to QR already made.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import '../src/App.css';
 import Home from "./pages/Home.js"
 import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound';
-import Estone from "./pages/Estone.js"
+import Estone2 from "./pages/Estone2.js"
+import Estone from './pages/Estone.js'
 import {v4 as uuidv4} from 'uuid';
 
 
@@ -31,7 +32,7 @@ const [stone, setStone] = useState({});
             <Route path="/" element={<Home setStone={setStone} loggedIn={loggedIn} setLoggedIn={setLoggedIn} userID={userID} />} />
             <Route path="login" element={<Login setUserID={setUserID} setLoggedIn={setLoggedIn}/>}/>
              {/* atob() base64 encoder */}
-            {/* <Route path="/e-stones/example" element={<Estone />} ></Route> */}
+            <Route path="/e-stones/example" element={<Estone2 />} ></Route>
             <Route path={`/:personID`} element={<Estone stone={stone} setStone={setStone}/>} />
             <Route path='/*' element={<NotFound />} />
           </Routes>
---
+++ src/pages/Estone2.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import "./Estone.css";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import Video from "../components/video_example.js"
+
+class Estone2 extends Component {
+
+    render() {
+
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col sm={12} xl >
+                        <Card style={{ width: '100%' }}>
+                            <Card.Img variant="top" src="https://t3.ftcdn.net/jpg/02/58/21/50/360_F_258215071_9LsJ4zATYzIj92rKC9pOLt7qiJA3u7lt.jpg" />
+                            <Card.Body>
+                                <Card.Title className="text-center">Susie Wolfe</Card.Title>
+                                <iframe
+                                    width="100%"
+                                    height="400px"
+                                    src="https://www.youtube.com/embed/fhflUhBVGko?controls=0"
+                                    title="Susie Wolfe Epitaph">
+                                </iframe>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    };
+};
+
+
+export default Estone2;
+
